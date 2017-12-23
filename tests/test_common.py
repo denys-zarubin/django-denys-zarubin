4a6213d99cb1a5594bbbdb7cd9a9dca43528a85c
den <den99f@me.com>
2017-12-23 09:51:04 +0200

    - Add first test for health check.

---
+++ tests/test_common.py
@@ -0,0 +1,11 @@
+from django.urls import reverse
+from rest_framework.test import APITestCase
+
+
+class HealthCheckTests(APITestCase):
+    def test_ok(self):
+        url = reverse('health_check')
+        response = self.client.get(
+            url
+        )
+        self.assertEqual(response.status_code, 200)
